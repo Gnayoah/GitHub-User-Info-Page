6ed7de4d16c2020d5dd34688ddefc8e3f991e415
Gnayoah <haoyang@gnayoah.com>
2024-06-08 11:15:33 -0700

    Update config.js

--- html/config.js
+++ html/config.js
@@ -1,1 +1,5 @@
-const githubUser = 'Gnayoah';  // Change this to the GitHub username you want to display
+// Change this to the GitHub username you want to display
+const githubUser = 'Gnayoah';  
+
+// Change the footer content here
+const footerContent = 'AGPL-3.0 License | Your footer Content';  
